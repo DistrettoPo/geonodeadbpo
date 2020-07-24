25ac39bc2cff04c409b40b7ae43b7761dad24de4
abulojoshua1 <abulojoshua1@gmail.com>
2020-07-24 13:17:03 +0300

    [Fixes #60] Test custom ADBPO theme context

--- adbpo_geonode/themes/tests.py
+++ adbpo_geonode/themes/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from adbpo_geonode.themes.context_processors import custom_theme
+from adbpo_geonode.themes.models import (ADBPOCustomGeonodeTheme,
+                                         ADBPOJumbotronSlideShow)
+from geonode.tests.base import GeoNodeBaseTestSupport
 
-# Create your tests here.
+
+class TestADBPOCustomTheme(GeoNodeBaseTestSupport):
+    def setUp(self):
+        super(TestADBPOCustomTheme, self).setUp()
+
+    def test_context_with_empty_theme(self):
+        theme_context = custom_theme(None)
+        self.assertEqual(
+            theme_context, {'custom_theme': {}, 'slide_count': 0, 'slideshow': []})
+
+    def test_context_with_theme(self):
+        slide_show = ADBPOJumbotronSlideShow.objects.create(
+            slide_name='test-slide')
+        theme = ADBPOCustomGeonodeTheme.objects.create(
+            name='test-theme', is_enabled=True)
+        theme.jumbotron_slide_show.add(slide_show)
+        theme_context = custom_theme(None)
+        self.assertEqual(theme_context, {
+            'custom_theme': theme,
+            'slide_count': 1,
+            'slideshow': [{
+                'index': 0,
+                'slide': slide_show
+            }]
+        })
